6e128f4127f4f44df033c8a6626fcc4a14c90640
Maeva DTC <delruemaeva@orange.fr>
2024-09-12 17:42:00 +0200

    added Tab and Stack config

--- app/(app)/(home)/_layout.js
+++ app/(app)/(home)/_layout.js
@@ -1,5 +1,9 @@
 import { Stack } from "expo-router";
 
 export default AppLayout = () => {
-  return <Stack />;
+  return (
+    <Stack>
+      <Stack.Screen name="index" options={{ title: "jsp" }} />
+    </Stack>
+  );
 };
--- app/(app)/_layout.js
+++ app/(app)/_layout.js
@@ -1,5 +1,35 @@
 import { Tabs } from "expo-router";
 
 export default AppLayout = () => {
-  return <Tabs />;
+  return (
+    <Tabs>
+      <Tabs.Screen
+        name="home"
+        options={{
+          title: "Home",
+          tabBarIcon: ({ color }) => (
+            <Ionicons name="home" size={24} color={color} />
+          ),
+        }}
+      />
+      <Tabs.Screen
+        name="map"
+        options={{
+          title: "Map",
+          tabBarIcon: ({ color }) => (
+            <Ionicons name="person" size={24} color={color} />
+          ),
+        }}
+      />
+      <Tabs.Screen
+        name="profile"
+        options={{
+          title: "Profil",
+          tabBarIcon: ({ color }) => (
+            <Ionicons name="person" size={24} color={color} />
+          ),
+        }}
+      />
+    </Tabs>
+  );
 };
